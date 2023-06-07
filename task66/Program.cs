ff80c2c93a90667a8fb341c7d6388049186d65d2
Ovod2013 <Ovod2013@yandex.ru>
2023-06-08 01:03:49 +0300

    Задача 66 работает

--- task66/Program.cs
+++ task66/Program.cs
@@ -11,4 +11,36 @@ int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Сумма натуральных чисел в промежутке от M до N: ");
+if (n < m) 
+{
+    Console.WriteLine ("Число N должно быть больше или равно M");
+}
+while (n < m)
+{
+Console.Write("Введите число N: ");
+n = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n}:");
+
+int summaNM = SummaNat(m, n);
+Console.WriteLine(summaNM);
+
+int SummaNat(int m,int n)
+{
+    int summa = n;
+    if (n == m) 
+    {
+        return summa;
+    }
+else 
+{
+    summa=n + SummaNat(m, n-1);
+    return summa;
+}
+}
+
+
+
+
+
