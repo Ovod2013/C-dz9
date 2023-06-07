1e91ea635903325b92bc692d5b0b30391f72dece
Ovod2013 <Ovod2013@yandex.ru>
2023-06-07 23:15:10 +0300

    Задача 68 работает

---
+++ task67/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write(SumNumbers(number));
+
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    Console.WriteLine(number);
+    return number%10 + SumNumbers(number/10);  
+}
---
+++ task68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления 
+// функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// A(m, n) = n + 1, если m = 0,
+// A(m, n) = A(m - 1, 1), если m <> 0, n = 0,
+// A(m, n) = A(m - 1, A(m, n - 1)), если m> 0, n > 0.
+
+Console.Write("Введите число m не более 3: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(akkerman(m, n));
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+return akkerman(m - 1, akkerman(m, n - 1));
+}
